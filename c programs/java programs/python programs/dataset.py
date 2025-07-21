c3412471993703657ab6d941bb69c114b7fef141
Reddy1236 <2400033275@kluniversity.in>
2025-07-21 17:16:39 +0530

    Added more files

---
+++ c programs/java programs/python programs/dataset.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv("student.csv")
+
+# Print the data
+print("✅ CSV File Loaded!")
+print(df)
---
+++ c programs/java programs/python programs/first.py
@@ -0,0 +1,11 @@
+# This program adds two numbers entered by the user
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add numbers
+sum = num1 + num2
+1
+# Display result
+print("The sum is:", sum)
