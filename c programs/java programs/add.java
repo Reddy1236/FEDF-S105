c0065f32b422770761c3ae0c98c859d5651ae4cd
Reddy1236 <2400033275@kluniversity.in>
2025-07-23 07:38:27 +0530

    frontend

--- c programs/java programs/add.java
+++
@@ -1,7 +0,0 @@
-public class add {
-    public static void main(String[] args){
-    int a=9,b=1;
-    int s=a+b;
-    System.out.println("Sum is : "+s);
-    
-}}
--- c programs/java programs/python programs/dataset.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv("student.csv")
-
-# Print the data
-print("✅ CSV File Loaded!")
-print(df)
